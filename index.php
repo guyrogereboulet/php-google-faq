f8ee1a13c5e2da4c87d242bec2744b68fa7f5bf8
Guy Roger Eboulet <eboulet87@gmail.com>
2020-02-24 14:55:40 +0100

    Header

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <style media="screen">
+
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+
+      .header-bottom {
+        border-bottom: 1px solid grey;
+        padding-bottom: 20px;
+      }
+
+      .header-bottom ul {
+        list-style: none;
+      }
+
+      ul.header-bottom-list {
+        list-style: none;
+        display: flex;
+        flex-direction: row;
+      }
+
+      ul.header-bottom-list li {
+        padding-right: 20px;
+      }
+    </style>
+    <title>php-google-faq</title>
+  </head>
+  <body>
+    <header>
+      <div class="header-top">
+        <img alt="Google" class="MntwIc" src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png">
+
+      </div>
+      <div class="header-bottom">
+        <ul class="header-bottom-list">
+          <li><a href="#">Introduzione</a></li>
+          <li><a href="#">Norme sulla privacy</a></li>
+          <li><a href="#">Termini di servizio</a></li>
+          <li><a href="#">Tecnologie</a></li>
+          <li><a href="#">Domande frequenti</a></li>
+          <li><a href="#">Account Google</a></li>
+        </ul>
+
+      </div>
+
+    </header>
+
+  </body>
+</html>
